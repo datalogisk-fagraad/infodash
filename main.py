64139e8a9a9c42486b1d54ff7476d4ddd7700995
Truls Asheim <truls@asheim.dk>
2014-10-14 00:36:27 +0200

    Make killable by Ctrl+C

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ from PyQt5.QtCore import QUrl
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     engine = QQmlApplicationEngine('main.qml')
+
+    # Killable by Ctrl+C
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
     sys.exit(app.exec_())
 
